6645501c15e82c84dcdeadd7ae4f8873de351ad9
rdehnhardt <renatotkd@gmail.com>
2015-07-17 15:51:33 -0300

    Add print screens

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -13,6 +13,8 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
+    public $redirectPath = '/back';
+
     /**
      * Create a new authentication controller instance.
      *
