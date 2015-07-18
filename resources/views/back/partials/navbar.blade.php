3dd38042f6fae16badca281fe1481ee6557cf3da
Evandro Ribeiro <ribeiroevandro@live.com>
2015-07-18 13:57:47 -0300

    Adding support gravatar

--- resources/views/back/partials/navbar.blade.php
+++ resources/views/back/partials/navbar.blade.php
@@ -29,7 +29,7 @@
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                       aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                       aria-expanded="false"><img src="{{ Auth::user()->gravatar }}" alt="avatar" class="img-circle"> {{ Auth::user()->name }} <span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li><a href="/auth/logout">Logout</a></li>
                     </ul>
