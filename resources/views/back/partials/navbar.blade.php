afa71e15c48d4cae7f7246ac181556eedb5d1b0f
Renato Dehnhardt <renatotkd@gmail.com>
2015-07-21 00:30:37 -0300

    Change gravatar navbar 60 to 40

--- resources/views/back/partials/navbar.blade.php
+++ resources/views/back/partials/navbar.blade.php
@@ -38,7 +38,7 @@
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                        <img src="{{ Auth::user()->gravatar(60) }}" alt="avatar" class="img-circle profile-image"> {{ Auth::user()->name }} <span class="caret"></span>
+                        <img src="{{ Auth::user()->gravatar(40) }}" alt="avatar" class="img-circle profile-image"> {{ Auth::user()->name }} <span class="caret"></span>
                     </a>
                     <ul class="dropdown-menu">
                         <li><a href="{{ route('back.profile.index') }}">{{ trans('dictionary.profile') }}</a></li>
