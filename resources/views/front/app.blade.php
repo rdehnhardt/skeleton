32cd0bb6deb8a2616dfc64f78a3d7983a5d8796a
rdehnhardt <renatotkd@gmail.com>
2015-07-27 12:11:45 -0300

    Change README

--- resources/views/front/app.blade.php
+++ resources/views/front/app.blade.php
@@ -17,6 +17,19 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <script>
+        var _bc = _bc || [];
+
+        (function () {
+            var bc = document.createElement('script');
+            bc.src = '/analytics.js';
+            bc.type = 'text/javascript';
+            bc.async = true;
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(bc, s);
+        })();
+    </script>
 </head>
 
 <body>
