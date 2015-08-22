ea6ca3e5fd5328540d1ca35f15d281a15bdb055b
Renato Dehnhardt <renatotkd@gmail.com>
2015-08-21 21:59:06 -0300

    Add page title to home

--- app/Front/resources/views/scope/home.blade.php
+++ app/Front/resources/views/scope/home.blade.php
@@ -1,5 +1,7 @@
 @extends('front::app')
 
+@section('page-title', 'Home')
+
 @section('content')
     <div class="title">{{ config('app.name') }}</div>
     <a href="/auth/login">{{ trans('dictionary.restricted-area') }}</a>
