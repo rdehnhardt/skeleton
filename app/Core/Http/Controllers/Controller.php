df6fe988116245c688b4f02274c24d25c53eb439
Renato Dehnhardt <renatotkd@gmail.com>
2015-10-22 00:50:34 -0200

    Change config/app.php

--- app/Core/Http/Controllers/Controller.php
+++ app/Core/Http/Controllers/Controller.php
@@ -5,10 +5,9 @@ namespace App\Core\Http\Controllers;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Artesaos\SEOTools\Traits\SEOTools as UseSEOToolsTrait;
 use App\Core\Traits\UseFlash as UseFlashTrait;
 
 abstract class Controller extends BaseController
 {
-    use DispatchesJobs, ValidatesRequests, UseSEOToolsTrait, UseFlashTrait;
+    use DispatchesJobs, ValidatesRequests, UseFlashTrait;
 }
