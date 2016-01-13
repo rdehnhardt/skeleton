6840f8fab140a428b00e8c1522e6bb052fe51189
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2016-01-13 09:47:04 -0200

    Elixir per modules

--- app/Back/gulpfile.js
+++ app/Back/gulpfile.js
@@ -2,4 +2,8 @@ var elixir = require('laravel-elixir');
 
 elixir(function (mix) {
     mix.sass('back.scss', '../../public/css/');
+
+    mix.version([
+        '../../../public/css/back.css'
+    ], '../../public/build');
 });
--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,7 @@ var chug = require('gulp-chug');
 
 gulp.task('default', function () {
     gulp.src([
-        './app/Auth/gulpfile.js',
         './app/Back/gulpfile.js',
-        './app/Guest/gulpfile.js',
     ]).pipe(chug())
 });
+
