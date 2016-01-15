efc7475c85c61d2e863d0dd318a47537c2fd7121
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2016-01-15 16:07:12 -0200

    Elixir per modules

--- app/Back/resources/gulpfile.js
+++ app/Back/resources/gulpfile.js
@@ -1,23 +1,29 @@
 var elixir = require('laravel-elixir');
 require('laravel-elixir-imagemin');
 
+var root = './../../../';
+var module = '/back';
+
 // Configs
-elixir.config.publicPath = './../../../public';
+elixir.config.publicPath = root + 'public' + module;
 elixir.config.assetsPath = 'assets';
+elixir.config.production = true;
 elixir.config.images = {
-    outputFolder: elixir.config.publicPath + '/build/back/images',
+    outputFolder: elixir.config.publicPath + '/build/images',
     folder: elixir.config.assetsPath + '/images'
 };
 
 // Elixir
 elixir(function (mix) {
-    mix.sass('back.scss');
+    mix.sass('app.scss');
 
-    mix.version(['css/back.css']);
+    mix.version(['css/app.css']);
 
     mix.imagemin({
         optimizationLevel: 3,
         progressive: true,
         interlaced: true
     });
+
+    mix.copy(root + 'bower_components/font-awesome/fonts', elixir.config.publicPath + '/build/fonts/');
 });
