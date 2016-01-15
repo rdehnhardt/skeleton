247361182a9e08549499590ad23ae4a4e8657536
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2016-01-15 16:13:29 -0200

    Elixir per modules

--- app/Back/resources/gulpfile.js
+++ app/Back/resources/gulpfile.js
@@ -5,19 +5,19 @@ var root = './../../../';
 var module = '/back';
 
 // Configs
-elixir.config.publicPath = root + 'public' + module;
+elixir.config.publicPath = root + 'public';
 elixir.config.assetsPath = 'assets';
 elixir.config.production = true;
 elixir.config.images = {
-    outputFolder: elixir.config.publicPath + '/build/images',
+    outputFolder: elixir.config.publicPath + '/build/images' + module,
     folder: elixir.config.assetsPath + '/images'
 };
 
 // Elixir
 elixir(function (mix) {
-    mix.sass('app.scss');
+    mix.sass('back.scss');
 
-    mix.version(['css/app.css']);
+    mix.version(['css/back.css']);
 
     mix.imagemin({
         optimizationLevel: 3,
