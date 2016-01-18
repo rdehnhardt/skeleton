6c5dedf87b1f53c544af191632f4a5451f9805b5
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2016-01-18 15:31:23 -0200

    Elixir per modules

--- app/Back/resources/views/app.blade.php
+++ app/Back/resources/views/app.blade.php
@@ -14,8 +14,38 @@
 </head>
 
 <body>
-    @include('back::navigation')
+    <nav class="navbar navbar-light bg-white">
+        <a class="navbar-brand" href="#">Skeleton</a>
+
+        <ul class="nav navbar-nav pull-xs-right pull-sm-right pull-md-right pull-lg-right">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">
+                    <i class="fa fa-home"></i> Home <span class="sr-only">(current)</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+        </ul>
+    </nav>
 
     @yield('content')
+
+    <footer>
+        <div class="container-fluid">
+            <div class="text-xs-center text-sm-center text-md-right text-lg-right">
+                powered by <a href="https://github.com/rdehnhardt" target="_blank">rdehnhardt</a>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
