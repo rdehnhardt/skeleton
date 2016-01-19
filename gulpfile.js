0d3ee4562ec5aa4dd79f28791f4326faf889ea3d
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2016-01-19 11:45:35 -0200

    Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp');
 var chug = require('gulp-chug');
 
 gulp.task('default', function () {
-    gulp.src([
-        './app/Back/resources/gulpfile.js',
-    ]).pipe(chug())
+    var modules = [
+        './app/Back/resources/gulpfile.js'
+    ];
+
+    gulp.src(modules).pipe(chug());
 });
 
