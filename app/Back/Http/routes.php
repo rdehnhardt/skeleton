cb702267afbf9d99f28e7ee94c1e1e4bc2422d6d
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2016-02-05 15:11:51 -0200

    Auth

--- app/Back/Http/routes.php
+++ app/Back/Http/routes.php
@@ -1,34 +1,13 @@
 <?php
 
-$actions = DB::select('select * from actions');
+Route::get('/', ['as' => 'back', 'uses' => 'Dashboard\DefaultController@index']);
 
-foreach ($actions as $action) {
-    if (strtoupper(trim($action->type)) === 'GET') {
-        Route::get($action->uri, ['as' => $action->name, 'uses' => $action->controller]);
-    }
+Route::group(['prefix' => 'dashboard', 'namespace' => 'Dashboard'], function () {
+    Route::get('/', ['as' => 'back.dashboard', 'uses' => 'DefaultController@index']);
+    Route::get('/visits/{start}/{end}', ['as' => 'back.dashboard.visits', 'uses' => 'DefaultController@visits']);
+    Route::get('/components/{component?}', ['as' => 'back.dashboard.components', 'uses' => 'ComponentsController@index']);
+});
 
-//    if (strtoupper(trim($action->type)) === 'POST') {
-//        Route::post($action->uri, ['as' => $action->name, 'uses' => $action->controller]);
-//    }
-//
-//    if (strtoupper(trim($action->type)) === 'PUT') {
-//        Route::put($action->uri, ['as' => $action->name, 'uses' => $action->controller]);
-//    }
-//
-//    if (strtoupper(trim($action->type)) === 'DELETE') {
-//        Route::delete($action->uri, ['as' => $action->name, 'uses' => $action->controller]);
-//    }
-}
-
-//Route::get('/', ['as' => 'back', 'uses' => 'Dashboard\DefaultController@index']);
-//
-//Route::group(['prefix' => 'dashboard', 'namespace' => 'Dashboard'], function () {
-//    Route::get('/', ['as' => 'back.dashboard', 'uses' => 'DefaultController@index']);
-//    Route::get('/visits/{start}/{end}', ['as' => 'back.dashboard.visits', 'uses' => 'DefaultController@visits']);
-//    Route::get('/components/{component?}', ['as' => 'back.dashboard.components', 'uses' => 'ComponentsController@index']);
-//});
-//
-//Route::group(['prefix' => 'system', 'namespace' => 'System'], function () {
-//    Route::resource('users', 'UsersController');
-//    Route::resource('actions', 'ActionsController');
-//});
+Route::group(['prefix' => 'system', 'namespace' => 'System'], function () {
+    Route::resource('users', 'UsersController');
+});
