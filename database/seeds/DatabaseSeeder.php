2ead2571143ec97f6557aafdacba6eb93646af5a
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2016-02-12 17:14:48 -0200

    Police

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        $this->call(ActionsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
 
         Model::reguard();
