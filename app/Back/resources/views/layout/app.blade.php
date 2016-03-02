4070e9d574dbf2cfae27b2d47cd4a38bfa360fe3
rdehnhardt <renatotkd@gmail.com>
2016-03-02 19:51:42 -0300

    Set Theme Color

--- app/Back/resources/views/layout/app.blade.php
+++ app/Back/resources/views/layout/app.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>{{ config('app.name') }}</title>
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta charset="utf-8" />
+    <meta name="theme-color" content="#3C8DBC" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport" />
     <link rel="stylesheet" href="{{ elixir('css/back.css') }}">
 
     <!--[if lt IE 9]>
