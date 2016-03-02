2881bbb689975e2744274a6cc511da52bd2a520b
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2016-03-02 18:05:07 -0300

    add artesaos/seotools

---
+++ config/seotools.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    'meta' => [
+        /*
+         * The default configurations to be used by the meta generator.
+         */
+        'defaults' => [
+            'title' => "It's Over 9000!", // set false to total remove
+            'description' => 'This is a app skeleton with website analytics. To use this project, make a fork', // set false to total remove
+            'separator' => ' - ',
+            'keywords' => [],
+        ],
+
+        /*
+         * Webmaster tags are always added.
+         */
+        'webmaster_tags' => [
+            'google' => null,
+            'bing' => null,
+            'alexa' => null,
+            'pinterest' => null,
+            'yandex' => null,
+        ],
+    ],
+    'opengraph' => [
+        /*
+         * The default configurations to be used by the opengraph generator.
+         */
+        'defaults' => [
+            'title' => 'Over 9000 Thousand!', // set false to total remove
+            'description' => 'For those who helped create the Genki Dama', // set false to total remove
+            'url' => false,
+            'type' => false,
+            'site_name' => false,
+            'images' => [],
+        ],
+    ],
+    'twitter' => [
+        /*
+         * The default values to be used by the twitter cards generator.
+         */
+        'defaults' => [
+            //'card'        => 'summary',
+            //'site'        => '@LuizVinicius73',
+        ],
+    ],
+];
