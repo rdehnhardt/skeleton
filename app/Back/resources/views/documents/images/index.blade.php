9524230ea4f32c213bc700f3ef49f4593112a5a8
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2016-03-23 13:12:37 -0300

    FIX user list role

--- app/Back/resources/views/documents/images/index.blade.php
+++ app/Back/resources/views/documents/images/index.blade.php
@@ -16,6 +16,10 @@
             <div id="grid" data-columns>
                 <div class="image">
                     <img src="http://ultraimg.com/images/photo-1.jpg" class="img-responsive"/>
+                    <p class="tags">
+                        <span class="badge">montain</span>
+                        <span class="badge">clouds</span>
+                    </p>
                     <div class="actions">
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-pencil"></i> Edit</a>
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-crop"></i> Crop</a>
@@ -25,6 +29,10 @@
                 </div>
                 <div class="image">
                     <img src="http://ultraimg.com/images/photo-2.jpg" class="img-responsive"/>
+                    <p class="tags">
+                        <span class="badge">car</span>
+                        <span class="badge">old car</span>
+                    </p>
                     <div class="actions">
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-pencil"></i> Edit</a>
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-crop"></i> Crop</a>
@@ -34,6 +42,10 @@
                 </div>
                 <div class="image">
                     <img src="http://ultraimg.com/images/photo-3.jpg" class="img-responsive"/>
+                    <p class="tags">
+                        <span class="badge">red fruits</span>
+                        <span class="badge">colors</span>
+                    </p>
                     <div class="actions">
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-pencil"></i> Edit</a>
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-crop"></i> Crop</a>
@@ -43,6 +55,10 @@
                 </div>
                 <div class="image">
                     <img src="http://ultraimg.com/images/photo-43eec3.jpg" class="img-responsive"/>
+                    <p class="tags">
+                        <span class="badge">ocean</span>
+                        <span class="badge">blue</span>
+                    </p>
                     <div class="actions">
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-pencil"></i> Edit</a>
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-crop"></i> Crop</a>
@@ -52,6 +68,10 @@
                 </div>
                 <div class="image">
                     <img src="http://ultraimg.com/images/photo-5.jpg" class="img-responsive"/>
+                    <p class="tags">
+                        <span class="badge">florest</span>
+                        <span class="badge">blue</span>
+                    </p>
                     <div class="actions">
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-pencil"></i> Edit</a>
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-crop"></i> Crop</a>
@@ -61,6 +81,10 @@
                 </div>
                 <div class="image">
                     <img src="http://ultraimg.com/images/photo-6.jpg" class="img-responsive"/>
+                    <p class="tags">
+                        <span class="badge">bamboo</span>
+                        <span class="badge">green</span>
+                    </p>
                     <div class="actions">
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-pencil"></i> Edit</a>
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-crop"></i> Crop</a>
@@ -70,6 +94,10 @@
                 </div>
                 <div class="image">
                     <img src="http://ultraimg.com/images/photo-7.jpg" class="img-responsive"/>
+                    <p class="tags">
+                        <span class="badge">read</span>
+                        <span class="badge">woman</span>
+                    </p>
                     <div class="actions">
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-pencil"></i> Edit</a>
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-crop"></i> Crop</a>
@@ -79,6 +107,11 @@
                 </div>
                 <div class="image">
                     <img src="http://ultraimg.com/images/photo-8.jpg" class="img-responsive"/>
+                    <p class="tags">
+                        <span class="badge">boat</span>
+                        <span class="badge">river</span>
+                        <span class="badge">bridge</span>
+                    </p>
                     <div class="actions">
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-pencil"></i> Edit</a>
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-crop"></i> Crop</a>
@@ -88,6 +121,11 @@
                 </div>
                 <div class="image">
                     <img src="http://ultraimg.com/images/photo-9.jpg" class="img-responsive"/>
+                    <p class="tags">
+                        <span class="badge">san francisco</span>
+                        <span class="badge">river</span>
+                        <span class="badge">bridge</span>
+                    </p>
                     <div class="actions">
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-pencil"></i> Edit</a>
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-crop"></i> Crop</a>
@@ -97,6 +135,10 @@
                 </div>
                 <div class="image">
                     <img src="http://ultraimg.com/images/photo-10.jpg" class="img-responsive"/>
+                    <p class="tags">
+                        <span class="badge">new york</span>
+                        <span class="badge">sky</span>
+                    </p>
                     <div class="actions">
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-pencil"></i> Edit</a>
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-crop"></i> Crop</a>
@@ -106,6 +148,10 @@
                 </div>
                 <div class="image">
                     <img src="http://ultraimg.com/images/photo-11.jpg" class="img-responsive"/>
+                    <p class="tags">
+                        <span class="badge">island</span>
+                        <span class="badge">ocean</span>
+                    </p>
                     <div class="actions">
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-pencil"></i> Edit</a>
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-crop"></i> Crop</a>
@@ -115,6 +161,10 @@
                 </div>
                 <div class="image">
                     <img src="http://ultraimg.com/images/photo-12.jpg" class="img-responsive"/>
+                    <p class="tags">
+                        <span class="badge">clouds</span>
+                        <span class="badge">montain</span>
+                    </p>
                     <div class="actions">
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-pencil"></i> Edit</a>
                         <a href="#" class="btn btn-default btn-xs"><i class="fa fa-crop"></i> Crop</a>
