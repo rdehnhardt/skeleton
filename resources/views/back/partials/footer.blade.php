b501b31917480fb6a2dc56c11c5e8f1bf38380c3
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2017-04-04 07:35:33 -0300

    Change provider

--- resources/views/back/partials/footer.blade.php
+++ resources/views/back/partials/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs"><b>Version</b> 5.3</div>
-    <strong>Copyright &copy; 2014 - {{ date('Y') }} <a href="https://github.com/baconfy/">baconfy</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2014 - {{ date('Y') }} <a href="https://github.com/rdehnhardt/">rdehnhardt</a>.</strong> All rights reserved.
 </footer>
