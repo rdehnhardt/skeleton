11b035be9d29133a465ddcfa7e9e6ff2dce89e2d
vagrant <vagrant@homestead>
2017-04-14 00:33:57 +0000

    Remove old Skeleton

--- resources/views/back/partials/footer.blade.php
+++
@@ -1,4 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs"><b>Version</b> 5.3</div>
-    <strong>Copyright &copy; 2014 - {{ date('Y') }} <a href="https://github.com/rdehnhardt/">rdehnhardt</a>.</strong> All rights reserved.
-</footer>
--- resources/views/back/partials/messages.blade.php
+++
@@ -1,7 +0,0 @@
-@include('flash::message')
-
-@if (Session::has('message') && Session::get('message-class'))
-    <div class="alert alert-{!! Session::get('message-class') !!}">
-        <p>{{ Session::get('message') }}</p>
-    </div>
-@endif
