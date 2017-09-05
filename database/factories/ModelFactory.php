b35e7c89efd2c2524d065a3c73ea7d149c3fb5e7
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2017-09-05 09:07:55 -0300

    Add template

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\User::class, function (Faker $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+});
