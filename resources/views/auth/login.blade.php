6345460c6d3247715137b36d95a9e81df1594181
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2017-09-05 08:23:58 -0300

    Add template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,47 +3,36 @@
 @section('title', 'Login')
 
 @section('content')
-    <form role="form" method="POST" action="{{ route('login') }}">
-        {{ csrf_field() }}
-
-        <div class="card card-hidden">
+    {!! Form::open(['method' => 'post', 'route' => ['login']]) !!}
+        <div class="card {{ !$errors->isEmpty() ?: 'card-hidden' }}">
             <div class="header text-center">Acesso Restrito</div>
-            <div class="content padding-h-40">
-                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label for="email">Endereço de email</label>
-                    <input type="email" id="email" name="email" placeholder="Insira seu email" class="form-control" value="{{ old('email') }}">
-                    @if ($errors->has('email'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </span>
-                    @endif
-                </div>
 
-                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label for="password">Senha</label>
-                    <input type="password" id="password" name="password" placeholder="Senha" class="form-control">
-                    @if ($errors->has('password'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                    @endif
-                </div>
+            <div class="content padding-h-40">
+                {!! Form::openGroup('email', 'Email Address') !!}
+                {!! Form::email('email', null, ['placeholder' => 'Type your email address']) !!}
+                {!! Form::closeGroup() !!}
+
+                {!! Form::openGroup('password', 'Your Password') !!}
+                {!! Form::password('password', ['placeholder' => 'Type your password']) !!}
+                {!! Form::closeGroup() !!}
 
                 <div class="form-group">
                     <label class="checkbox">
                         <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }} data-toggle="checkbox">
-                        Lembrar Senha
+                        Remember Password
                     </label>
                 </div>
             </div>
+
             <div class="footer text-center">
                 <button type="submit" class="btn btn-fill btn-primary btn-wd">Acessar</button>
             </div>
+
             <div class="footer text-center">
                 <a href="{{ route('password.request') }}">Esqueceu sua senha?</a>
             </div>
         </div>
-    </form>
+    {!! Form::close() !!}
 @endsection
 
 @section('page-scripts')
