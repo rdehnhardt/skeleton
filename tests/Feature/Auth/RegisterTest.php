b99f2b77c5dad25b2105483a5fdd457d9d6ff854
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2017-09-05 09:45:18 -0300

    Add test for register

--- tests/Feature/Auth/RegisterTest.php
+++ tests/Feature/Auth/RegisterTest.php
@@ -24,5 +24,68 @@ class RegisterTest extends TestCase
 
         $response = $this->post(route('login'), ['email' => $user['email'], 'password' => 'secret']);
         $response->assertRedirect('/home');
+
+        $this->assertDatabaseHas('users', ['email' => $user['email']]);
+    }
+
+    /** @test */
+    public function a_user_can_not_register_without_email()
+    {
+        $user = [
+            'name' => $this->fake->name,
+            'password' => 'secret',
+            'password_confirmation' => 'secret',
+        ];
+
+        $response = $this->post(route('register'), $user);
+        $response->assertSessionHasErrors(['email']);
+
+        $this->assertDatabaseMissing('users', ['name' => $user['name']]);
+    }
+
+    /** @test */
+    public function a_user_can_not_register_without_password()
+    {
+        $user = [
+            'name' => $this->fake->name,
+            'email' => $this->fake->email,
+            'password_confirmation' => 'secret',
+        ];
+
+        $response = $this->post(route('register'), $user);
+        $response->assertSessionHasErrors(['password']);
+
+        $this->assertDatabaseMissing('users', ['email' => $user['email']]);
+    }
+
+    /** @test */
+    public function a_user_can_not_register_without_password_confirmation()
+    {
+        $user = [
+            'name' => $this->fake->name,
+            'email' => $this->fake->email,
+            'password' => 'secret',
+        ];
+
+        $response = $this->post(route('register'), $user);
+        $response->assertSessionHasErrors(['password']);
+
+        $this->assertDatabaseMissing('users', ['email' => $user['email']]);
+    }
+
+    /** @test */
+    public function a_user_can_not_register_with_different_passwords()
+    {
+        $user = [
+            'name' => $this->fake->name,
+            'email' => $this->fake->email,
+            'password' => 'anotherpassword',
+            'password_confirmation' => 'secret',
+        ];
+
+        $response = $this->post(route('register'), $user);
+        $response->assertSessionHasErrors(['password']);
+
+        $this->assertDatabaseMissing('users', ['email' => $user['email']]);
     }
 }
